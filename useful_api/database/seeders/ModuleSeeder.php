3b89ff84000830b363292d73cd9a275c965abd13
Handsomeboy990 <lauretchacha@gmail.com>
2025-10-16 14:18:36 +0100

    fix: ModuleSeeder

--- useful_api/database/seeders/ModuleSeeder.php
+++ useful_api/database/seeders/ModuleSeeder.php
@@ -15,10 +15,10 @@ class ModuleSeeder extends Seeder
     {
         DB::table('modules')->insert([
             ['name' => 'URL Shortener', 'description' => 'Raccourcir et gérer des liens'],
-            ['name' => 'Wallet', 'description' => 'Gestion du solde et transferts'],
-            ['name' => 'Marketplace', 'description' => 'Gestion de produits et commandes'],
-            ['name' => 'Time Tracker', 'description' => 'Suivi du temps et sessions'],
-            ['name' => 'Investment Tracker', 'description' => 'Suivi du portefeuille d’investissement'],
+            ['name' => 'Wallet', 'description' => 'Gestion du solde et des transferts'],
+            ['name' => 'Marketplace + Stock Manager', 'description' => 'Gestion de produits et commandes'],
+            ['name' => 'Time Tracker', 'description' => 'Suivi des sessions et durées'],
+            ['name' => 'Investment Tracker', 'description' => 'Gestion du portefeuille d’investissement'],
         ]);
     }
 
